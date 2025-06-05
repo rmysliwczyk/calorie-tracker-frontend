68befdf21b9211ed4b4e4fea40f0d88e00721944
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2025-06-05 23:29:04 +0200

    Add shared meal bugfixes and daypicker format change

--- src/pages/AddSharedMeal.tsx
+++ src/pages/AddSharedMeal.tsx
@@ -2,53 +2,78 @@ import { useNavigate, useSearchParams } from "react-router";
 import useFetch from "../hooks/useFetch";
 import usePost from "../hooks/usePost";
 import { useEffect, useState } from "react";
-import { Alert, Button, Grid} from "@mui/material";
+import { Alert, Button, CircularProgress, Grid } from "@mui/material";
 import dayjs from "dayjs";
 import type { Dayjs } from "dayjs";
 import { LocalizationProvider, MobileDatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export default function AddSharedMeal() {
-    const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const idsParam = searchParams.get('ids');
-    const idArray = idsParam?.split(',').map(Number).filter(n => !isNaN(n)) || [];
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const idsParam = searchParams.get("ids");
+  const idArray =
+    idsParam
+      ?.split(",")
+      .map(Number)
+      .filter((n) => !isNaN(n)) || [];
 
-    // Build the query string for multiple ids: ?ids=1&ids=2&ids=3
-    const [selectedDate, setSelectedDate] = useState<Dayjs|null>(dayjs())
-    const [confirmedAdd, setConfirmedAdd] = useState<boolean>(false);
-    const [searchUrl, setSearchUrl] = useState(`/meals/?${idArray.map(id => `ids=${id}`).join("&")}`)
-    const { data, error, loading } = useFetch<Meal[]>(searchUrl);
-    const { post } = usePost();
+  // Build the query string for multiple ids: ?ids=1&ids=2&ids=3
+  const [selectedDate, setSelectedDate] = useState<Dayjs | null>(dayjs());
+  const [confirmedAdd, setConfirmedAdd] = useState<boolean>(false);
+  const [searchUrl, setSearchUrl] = useState(
+    `/meals/?${idArray.map((id) => `ids=${id}`).join("&")}`
+  );
+  const { data, error: fetchError, loading: fetchLoading } = useFetch<Meal[]>(searchUrl);
+  const { post, data: postData, error: postError, loading: postLoading} = usePost();
 
-    useEffect(() => {
-        if(data && confirmedAdd == true && selectedDate) {
-            data.forEach((meal) => meal.created_at = selectedDate.format("YYYY-MM-DD"))
-            post("/meals/create-many", data)
-            setConfirmedAdd(true)
-            navigate("/meals")
-        }
-    }, [data, selectedDate, confirmedAdd]);
+  function addSharedMeal() {
+    if (data && selectedDate && !fetchError && !fetchLoading) {
+      data.forEach((meal) => (meal.created_at = selectedDate.format("YYYY-MM-DD")));
+      post("/meals/create-many", data);
+      setConfirmedAdd(true);
+    }
+  }
 
-    if (confirmedAdd === false) {
-        return(<Grid container>
-          <Grid size={12}>
+  useEffect(() => {
+    if (!postLoading && !postError && postData) {
+      navigate("/meals");
+    }
+  }, [postData, postLoading]);
+
+  if (fetchError || postError) {
+    return <Alert severity="error">Error: {fetchError || postError}</Alert>;
+  } else if (fetchLoading || postLoading) {
+    return <CircularProgress/>
+  } else if(!data || data.length == 0) {
+    return <Alert severity="error">Couldn't get the data for shared meals. Please get a new share meal link.</Alert>;
+  } else {
+    return (
+      <Grid container sx={{gap: "10px"}}>
+        <Grid size={12}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <MobileDatePicker
+              format="YYYY-MM-DD"
               value={selectedDate}
               onChange={function (newValue) {
                 setSelectedDate(newValue);
               }}
-              sx={{width: "80%"}}
+              sx={{ width: "80%" }}
               label="Select day"
             />
           </LocalizationProvider>
         </Grid>
         <Grid size={12}>
-          <Button onClick={function() {setConfirmedAdd(true)}}>Add meal</Button>
+          <Button
+            variant="contained"
+            onClick={function () {
+              addSharedMeal();
+            }}
+          >
+            Add shared meal
+          </Button>
         </Grid>
-        </Grid>
-        )
-    }
-    return null;
+      </Grid>
+    );
+  }
 }
